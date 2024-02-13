d1853feeaafc05001078dcb5a263b709f4507b99
Горбулёв Сергей <gorbulevsv@yandex.ru>
2024-02-13 21:34:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/androidukazania/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.androidukazania;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://svelte-ukazania.netlify.app/");
+
+        SwipeRefreshLayout swiper = findViewById(R.id.swiper);
+        swiper.setOnRefreshListener(() -> {
+            webView.loadUrl("https://svelte-ukazania.netlify.app/");
+            swiper.setRefreshing(false);
+        });
+
+
+    }
+}
